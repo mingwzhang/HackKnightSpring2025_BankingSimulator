ec61669774b25be7f9ebf20d80532ba67bf277c4
Carl Feng <carlfeng3@gmail.com>
2025-03-07 19:48:28 -0500

    Added dialogue and tablet components
    Tablet component and dialogue box added

---
+++ banking-simulator/src/app/components/dialoguebox.js
@@ -0,0 +1,12 @@
+
+function DialogueBox() {
+  return (
+    <div className="flex h-full w-full justify-center bg-blue-200 text-4xl">
+      <div className="flex items-center ml-5 mt-2 p-2 font-[ZZZFont]">
+        Hello, I would like to withdraw $50.
+      </div>
+    </div>
+  )
+}
+
+export default DialogueBox
