72fd8e199c5f1b39f9cd06a553eb7e5b8640b4f2
Mingwei Zhang <81440878+mingwzhang@users.noreply.github.com>
2025-03-09 04:53:33 -0400

    reverting back to http since c1 api only allows http

--- banking-simulator/src/app/game/page.js
+++ banking-simulator/src/app/game/page.js
@@ -19,7 +19,7 @@ export default function Home() {
 
   useEffect(() => {
     const fetchCustomersList = () => {
-      fetch(`https://api.nessieisreal.com/customers?key=${key}`)
+      fetch(`http://api.nessieisreal.com/customers?key=${key}`)
         .then(res => res.json())
         .then(data => { setCustomerList(data); console.log("Customer list updated") })
         .catch(err => console.log(err))
